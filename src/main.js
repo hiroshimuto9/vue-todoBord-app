788a62d0055c3fa58ea252ceceff22606ff28744
mutohiroshi <mutohiroshi@team-lab.com>
2019-09-05 12:56:44 +0900

    [setup] 環境構築

---
+++ src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'es6-promise/auto'
+import store from './store' // Vuexのストアインスタンスをインポート
+
+Vue.config.productionTip = process.env.NODE_ENV === 'production'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store, // インポートしたストアインスタンスをstoreオプションとして指定
+  components: { App },
+  template: '<App/>'
+})
